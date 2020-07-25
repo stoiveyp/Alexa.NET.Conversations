ec5b1e2652841983c1f5aa9507258caf019b31bf
Steven Pears <steven.pears@live.co.uk>
2020-07-25 13:01:11 +0100

    initial structure and test

---
+++ Alexa.NET.Conversations/DialogApiInvokedRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Conversations
+{
+    public class DialogApiInvokedRequest:Request.Type.Request
+    {
+        public static void AddToRequestConverter()
+        {
+            RequestConverter.RequestConverters.Add(new DialogApiRequestConverter());
+        }
+
+        [JsonProperty("apiRequest")]
+        public ApiRequest ApiRequest { get; set; }
+    }
+
+    public class ApiRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("arguments",NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string,object> Arguments { get; set; }
+
+        [JsonProperty("slots",NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string,SlotValue> Slots { get; set; }
+    }
+}

4c680ea7c783e611c3c5c8730143a66394e20869
Steven Pears <steven.pears@live.co.uk>
2020-07-25 17:33:05 +0100

    Add ApiResponse

---
+++ Alexa.NET.Conversations/DialogConversationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+
+namespace Alexa.NET.Conversations
+{
+    public class DialogConversationResponse:ResponseBody
+    {
+        [JsonProperty("apiResponse",NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string,object> ApiResponse { get; set; }
+    }
+}
